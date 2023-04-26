63eaf7fb20bdd1b4bfafa189675f50b5163e1c45
omar paredes <petitenfant2014@gmail.com>
2023-04-26 10:09:03 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import Accueil from './pages/Accueil/accueil';
+import Erreur from './pages/Erreur/erreur';
+import Apropos from './pages/Apropos/apropos';
+import Logement from './pages/Logement/logement';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const router = createBrowserRouter([
+	{
+		path: '/',
+		element: <Accueil />,
+	},
+	{
+		path: '/accueil',
+		element: <Accueil />,
+	},
+	{
+		path: '/logement',
+		element: <Logement />,
+	},
+	{
+		path: '/logement/:id',
+		element: <Logement />,
+	},
+	{
+		path: '/apropos',
+		element: <Apropos />,
+	},
+	{
+		path: '*',
+		element: <Erreur />,
+	},
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<RouterProvider router={router} />
+		</div>
+	);
 }
 
 export default App;
